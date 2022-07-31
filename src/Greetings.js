73d2fb67f862f9bb74bf70f2c70b5efde369e165
Ashish Sharda <ashishjsharda@gmail.com>
2022-07-31 17:20:15 -0500

    Add React Fragment

--- src/Greetings.js
+++ src/Greetings.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Greetings(){
     let isMorning=(new Date().getHours() <12);
-    return isMorning?<h3> Good Morning</h3> : <h3> Good Evening </h3>
+    return isMorning?
+        (<>
+            <h3> Good Morning</h3>
+            <p> You have unread new messages</p>
+            </>) : (<>
+            <h3> Good Evening </h3>
+            <p> You have unread new messages</p>
+            </>)
 }
 
 export default Greetings;

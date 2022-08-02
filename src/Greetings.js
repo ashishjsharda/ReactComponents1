f7778afb6ffbc4bb34834a7344fcc2767ee3ea99
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-01 21:36:04 -0500

    Code cleanup

--- src/Greetings.js
+++ src/Greetings.js
@@ -2,13 +2,11 @@ import React from "react";
 
 export const Greetings =() =>{
     let isMorning=(new Date().getHours() <12);
-    return isMorning?
-        (<>
-            <h3> Good Morning</h3>
-            <p> You have unread new messages</p>
-            </>) : (<>
-            <h3> Good Evening </h3>
-            <p> You have unread new messages</p>
-            </>)
+    let greetingHeader=isMorning?<h3>Good Morning</h3>:<h3>Good Evening</h3>;
+    return (
+        <>{greetingHeader}
+            <p> You have 5 new messages</p>
+        </>
+    )
 }
 

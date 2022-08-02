c1c08a9981f52030e9191dda8f9a085c13af6393
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-02 04:59:10 -0500

    Add props

--- src/Greetings.js
+++ src/Greetings.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Greetings =(props) =>{
     console.log(props);
     let isMorning=(new Date().getHours() <12);
-    let greetingHeader=isMorning?<h3>Good Morning</h3>:<h3>Good Evening</h3>;
+    let greetingHeader=isMorning?<h3>Good Morning {props.name}</h3>:<h3>Good Evening {props.name}</h3>;
     return (
         <>{greetingHeader}
             <p> You have 5 new messages</p>

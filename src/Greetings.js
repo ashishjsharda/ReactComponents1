ce10d321d52f4debe4919115622bfb9546dcf4ae
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-02 05:01:58 -0500

    Add object destructuring

--- src/Greetings.js
+++ src/Greetings.js
@@ -3,10 +3,11 @@ import React from "react";
 export const Greetings =(props) =>{
     console.log(props);
     let isMorning=(new Date().getHours() <12);
-    let greetingHeader=isMorning?<h3>Good Morning {props.name}</h3>:<h3>Good Evening {props.name}</h3>;
+    const {name,numberofMessages}=props;
+    let greetingHeader=isMorning?<h3>Good Morning {name}</h3>:<h3>Good Evening {name}</h3>;
     return (
         <>{greetingHeader}
-            <p> You have 5 new messages</p>
+            <p> You have {props.numberofMessages} new messages</p>
         </>
     )
 }

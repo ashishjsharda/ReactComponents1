6ca0b5adf247d2fab200a0757c639c58fce1ef82
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-03 20:16:50 -0500

    Props check

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greetings  numberofMessages={5}></Greetings>
+        <Greetings name={"Ashish"} numberofMessages={5}></Greetings>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

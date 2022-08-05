b74c4a86b3f3e5d3f090395aa53e1fadffe138dd
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-04 21:34:54 -0500

    Add condition for message

--- src/Greetings.js
+++ src/Greetings.js
@@ -7,7 +7,8 @@ export const Greetings =({name,numberofMessages}) =>{
     let greetingHeader=isMorning?<h3>Good Morning {name}</h3>:<h3>Good Evening {name}</h3>;
     return (
         <>{greetingHeader}
-            <p> You have {numberofMessages} new messages</p>
+            {numberofMessages===0 ?null :<p> You have {numberofMessages} new messages</p>}
+
         </>
     )
 }

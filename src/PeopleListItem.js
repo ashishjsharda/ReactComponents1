0ef4e7f55892128cdb13af6a9d3b31d113ad4af5
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-15 21:02:11 -0500

    Add css

--- src/PeopleListItem.js
+++ src/PeopleListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './PeopleListItem.css';
 export const PeopleListItem=({person}) => (
-    <div>
+    <div className="list-item-container">
         <h3>Name : {person.name}</h3>
         <p> Age :{person.age}</p>
         <p> HairColor :{person.hairColor}</p>

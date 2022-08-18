8a1d9ab6324994423a0122d31701270c263e284d
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-18 17:05:28 -0500

    Add a wrapper

--- src/PeopleList.js
+++ src/PeopleList.js
@@ -5,12 +5,13 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
 display: flex;
         border: 2px solid #ccc;
+        padding: 32px;
         
 `
 export const PeopleList=({people}) =>(
-    <>
+    <Wrapper>
         {people.map (person =><PeopleListItem person ={person} key={person.name} />)}
 
 
-        </>
+        </Wrapper>
 );

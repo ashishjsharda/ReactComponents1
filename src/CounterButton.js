2c01208aa13848e5d22f18056aad0171e61df05f
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-02 04:39:10 -0500

    Update useState

--- src/CounterButton.js
+++ src/CounterButton.js
@@ -1,9 +1,10 @@
 import React ,{useState}  from "react";
 export const CounterButton =() => {
     const [count,setCount]=useState(0);
+    const incrementCount=()=>setCount(count+1);
     return (
         <>
             <p>You have clicked the button {count} times</p>
-            <button>Click Me</button>
+            <button onClick={incrementCount}>Click Me</button>
         </>);
 }

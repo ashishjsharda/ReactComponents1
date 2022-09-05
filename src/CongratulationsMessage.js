eadaaf889aaabe59ac930d0abdd33f84f89f3da6
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-05 13:09:04 -0500

    Update congrats message

--- src/CongratulationsMessage.js
+++ src/CongratulationsMessage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export const CongratulationsMessage =({numberOfClicks,threshold}) =>{
     return numberOfClicks>= threshold
-        ?<h3>Congratulations ! You have reached {threshold} number of clicks!</h3>
+        ?<h3>Congratulations ! You have reached the threshold -  {threshold}  clicks!</h3>
         :null;
 
 };

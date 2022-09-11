2311edc41c8aa000b3f4cf73f827368cce19e570
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-11 05:33:50 -0500

    update threshold

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                {hideMessage ? null : <CongratulationsMessage numberOfClicks={numberOfClicks} threshold={10}/>}
+                {hideMessage ? null : <CongratulationsMessage numberOfClicks={numberOfClicks} threshold={5}/>}
 
                 <CounterButton onIncrement={incrementCount} numberOfClicks={numberOfClicks} />
             </header>

20ed77363b1ee233078dc2b7e55efbdd4180474c
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-11 05:43:56 -0500

    add useeffect

--- src/CongratulationsMessage.js
+++ src/CongratulationsMessage.js
@@ -1,5 +1,15 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 export const CongratulationsMessage =({numberOfClicks,threshold,onHide}) =>{
+
+   useEffect(()=>{
+         console.log('CongratulationsMessage useEffect called');
+         return ()=>{
+              console.log('Unsubscribe from CongratulationsMessage');
+         }
+
+   });
+
+
     return numberOfClicks>= threshold
         ?<>
         <h1>Congratulations ! You have reached the threshold -  {threshold}  clicks!</h1>

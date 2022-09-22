4885d91b66cf257a2cb241d55fa94a71e7bdb0bd
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-22 06:20:57 -0500

    UpdateCounterButtonPage

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {CounterButton} from "../CounterButton";
 import {CongratulationsMessage} from "../CongratulationsMessage";
 export const CounterButtonPage = () => {
+    const [numberOfClicks,setCount]=useState(0);
+    const incrementCount=()=>setCount(numberOfClicks+1);
+    const [hideMessage,setHideMessage]=useState(false);
+    return (
+        <div className="App">
+            <header className="App-header">
+                {hideMessage ? null : <CongratulationsMessage
+                    numberOfClicks={numberOfClicks}
+                    threshold={5}
+                    onHide={() =>setHideMessage(true)}/>}
 
+                <CounterButton onIncrement={incrementCount} numberOfClicks={numberOfClicks} />
+            </header>
+        </div>
+    );
 }

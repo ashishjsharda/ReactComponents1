076d9112ea2528400cc28560c2e80befb82f468a
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-25 09:01:40 -0500

    Update PeopleListPage

--- src/pages/PeopleListPage.js
+++ src/pages/PeopleListPage.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import {PeopleList} from "../PeopleList";
+const people=[
+    {
+        name:'Ashish',
+        age:40,
+        hairColor:'brown'
+    },{
+        name:'Jack',
+        age:42,
+        hairColor:'black'
+
+    }
+];
+
+export const PeopleListPage = () => (
+    <>
+        <h1>People List Page</h1>
+        <PeopleList people={people}/>
+    </>
+);

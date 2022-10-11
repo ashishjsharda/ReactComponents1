e7494a7fb6568f62763769d74b55d2f713ec1d25
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-11 04:45:10 -0500

    Add Navigate

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,7 +1,11 @@
 import React  from "react";
+import {Navigate} from "react-router-dom";
 export const ProtectedPage = () => {
-
-  return (
+const isAuth=false;
+  return isAuth
+       ? (
       <h1>Protected Page</h1>
+  ) : (
+        <Navigate to="/"/>
   );
 }

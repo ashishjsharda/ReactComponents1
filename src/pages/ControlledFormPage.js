8ee9da6fd73f42081cb33935bbd2030036e14848
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-16 05:49:03 -0500

    Update ControlledFormPage

--- src/pages/ControlledFormPage.js
+++ src/pages/ControlledFormPage.js
@@ -3,6 +3,16 @@ export const ControlledFormPage= () => {
 return(
     <form>
         <h3>Please enter your information</h3>
+        <div>
+            <input type="text" id="name" name="name" placeholder="Name" />
+
+        </div>
+        <div>
+            <input type="text" id="email" name="email" placeholder="Email" />
+        </div>
+        <div>
+            <input type="text"  name="favoritecolor" placeholder="Favorite Color" />
+        </div>
     </form>
 )
 }

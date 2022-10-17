b21285651114fb10e3e3a15de6aa18f4dc5c9533
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-17 05:30:46 -0500

    Add Hooks

--- src/pages/ControlledFormPage.js
+++ src/pages/ControlledFormPage.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 export const ControlledFormPage= () => {
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [favoriteColor,setFavoriteColor]=useState("");
 return(
     <form>
         <h3>Please enter your information</h3>
         <div>
-            <input type="text" id="name" name="name" placeholder="Name" />
+            <input type="text" id="name" name="name" placeholder="Name"
+            onChange={event => setName(event.target.value)}/>
 
         </div>
         <div>

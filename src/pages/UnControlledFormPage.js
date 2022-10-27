ea2b758888adce83719dc86f9de939fde638ea9d
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-27 06:23:19 -0500

    Add ref's

--- src/pages/UnControlledFormPage.js
+++ src/pages/UnControlledFormPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 export const UnControlledFormPage = () => {
+    const nameInputRef = React.useRef();
+    const emailInputRef = React.useRef();
+
     return (
         <div>
             <h1>UnControlled Form Page</h1>
             <form>
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Email" />
+                <input ref={nameInputRef} type="text" placeholder="Name" />
+                <input ref={emailInputRef} type="text" placeholder="Email" />
                 <input type="text" placeholder="Phone" />
                 <input type="text" placeholder="Address" />
                 <input type="submit" value="Submit" />

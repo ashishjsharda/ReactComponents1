3f02fa0eebb15b53ff3d8e939a59d7b9d1bd7bd9
Ashish Sharda <ashishjsharda@gmail.com>
2022-11-06 10:20:27 -0600

    update

--- src/App.js
+++ src/App.js
@@ -18,26 +18,22 @@ function App() {
                 <NavBar/>
                 <div className={"App-header"}>
 
-                <Link to="/">Home page</Link>
-                <Link to="/people-list">People list page</Link>
-                <Link to="/counter">Counter page</Link>
-                <Link to="/protected">Protected page</Link>
-                <Link to="/controlled-form">Controlled form page</Link>
-                <Link to="/uncontrolled-form">Uncontrolled form page</Link>
-                <Routes>
-                    <Route path="/" exact element={<HomePage />} />
-                    <Route path="/counter/" exact element={<CounterButtonPage />} />
-                    <Route path="/people-list" exact element={<PeopleListPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                    <Route path="/protected" element={<ProtectedPage />} />
-                    <Route path="/forms">
-                        <Router>
-                            <Route path="/controlled-form" element={<ControlledFormPage />} />
-                            <Route path="/uncontrolled-form" element={<UnControlledFormPage />} />
-                        </Router>
-                    </Route>
-                    
-                </Routes>
+                    <Link to="/">Home page</Link>
+                    <Link to="/people-list">People list page</Link>
+                    <Link to="/counter">Counter page</Link>
+                    <Link to="/protected">Protected page</Link>
+                    <Link to="/controlled-form">Controlled form page</Link>
+                    <Link to="/uncontrolled-form">Uncontrolled form page</Link>
+                    <Routes>
+                        <Route path="/" exact element={<HomePage />} />
+                        <Route path="/counter/" exact element={<CounterButtonPage />} />
+                        <Route path="/people-list" exact element={<PeopleListPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                        <Route path="/protected" element={<ProtectedPage />} />
+                        <Route path={"/controlled-form"} element={<ControlledFormPage />} />
+                        <Route path={"/uncontrolled-form"} element={<UnControlledFormPage />} />
+
+                    </Routes>
                 </div>
             </Router>
         </div>
